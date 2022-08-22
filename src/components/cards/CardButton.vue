861208b0e1d51eeab090b6a674873c7f281d5f38
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-22 20:01:52 +0200

    Change div into button

--- src/components/cards/CardButton.vue
+++ src/components/cards/CardButton.vue
@@ -1,10 +1,10 @@
 <template>
-  <div
+  <button
     class="p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
     bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
   >
     <div class="flex items-center justify-center flex items-center justify-center">
       <ArrowRightCircleIcon />
     </div>
-  </div>
+  </button>
 </template>
