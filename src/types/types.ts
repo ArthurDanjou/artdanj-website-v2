f71fa8771c404830aa22347624f364b0d9acc77c
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-22 20:01:38 +0200

    Add new types

---
+++ src/types/types.ts
@@ -0,0 +1,22 @@
+export interface SpotifyData {
+  is_playing: boolean
+  device_name: string
+  device_type: string
+  name: string
+  type: string
+  author: string
+  id: string
+  image: {
+
+    height: number
+    width: number
+    url: string
+
+  }
+  duration: number
+  started_at: number
+}
+
+export interface NewsletterForm {
+  email: string
+}
