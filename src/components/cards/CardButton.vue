89b7e43625dd50b5c62746646dfb58f41cb6e221
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-24 19:32:02 +0200

    Remake button structure

--- src/components/cards/CardButton.vue
+++ src/components/cards/CardButton.vue
@@ -1,10 +1,8 @@
 <template>
-  <button
-    class="p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
+  <div
+    class="flex items-center justify-center  p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
     bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
   >
-    <div class="flex items-center justify-center flex items-center justify-center">
-      <ArrowRightCircleIcon />
-    </div>
-  </button>
+    <ArrowRightCircleIcon />
+  </div>
 </template>
