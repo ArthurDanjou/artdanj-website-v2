92e1b9851146ae372d2539e1522cd978cf50a706
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-24 19:30:22 +0200

    Create template of blog post

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,14 +1,45 @@
 <script setup lang="ts">
-import { useThemeStore } from '~/store/theme'
-
-const themeStore = useThemeStore()
+import { useTheme } from '~/composables/useTheme'
+const { getTextColor } = useTheme()
 </script>
 
 <template>
-  <main>
+  <section>
     <PageTitle title="My Shelf" />
-    <Link target="_self" link="/blog/123" :color="themeStore.theme">
-      Lien 1
-    </Link>
-  </main>
+    <div class="flex justify-center">
+      <div class="grid grid-cols-2 gap-6 grid-flow-row-dense auto-">
+        <Card>
+          <CardLink href="/blog/123">
+            <h1 class="text-3xl font-bold" :class="getTextColor()">
+              TITLE
+            </h1>
+            <h3 class="text-sm text-stone-500 dark:text-gray-400 mt-2 text-justify">
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+              DESCRIPTION
+            </h3>
+            <div class="flex items-center justify-between mt-4">
+              <button
+                class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
+                bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
+              >
+                <div class="flex items-center justify-center flex items-center justify-center">
+                  Read more <PlusIcon class="ml-2" />
+                </div>
+              </button>
+              <p class="ml-4" :class="getTextColor()">
+                6 Feb. 2003
+              </p>
+            </div>
+          </CardLink>
+        </Card>
+      </div>
+    </div>
+  </section>
 </template>
