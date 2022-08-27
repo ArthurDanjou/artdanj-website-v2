7748d8441290e25b6d7836b267f3d55f8254ab21
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-27 20:21:50 +0200

    Add announce card

---
+++ src/components/cards/AnnounceCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useTheme } from '~/composables/useTheme'
+
+const { getTextColor } = useTheme()
+
+const { data } = await useAsyncData('announce', () => $fetch('/api/announces'))
+</script>
+
+<template>
+  <Card width="2">
+    <CardDiv>
+      <SpeakerIcon :class="getTextColor()" />
+      <h3 class="subtitle">
+        What are the latest news ?
+      </h3>
+      <h1 v-if="data" class="title">
+        {{ data.content }}
+      </h1>
+      <h1 v-else class="title">
+        Welcome to the website !
+      </h1>
+    </CardDiv>
+  </Card>
+</template>
