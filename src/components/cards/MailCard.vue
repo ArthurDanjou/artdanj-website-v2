15872216f1c579f324be8b1e1da536d906c0a979
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-28 19:19:31 +0200

    Add new Card

---
+++ src/components/cards/MailCard.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { useTheme } from '~/composables/useTheme'
+const { getTextColor } = useTheme()
+</script>
+
+<template>
+  <Card width="2">
+    <CardLink href="mailto:contact@arthurdanjou.fr">
+      <EmailIcon :class="getTextColor()" />
+      <h3 class="subtitle">
+        Just want to email me ?
+      </h3>
+      <h1 class="title">
+        contact@arthurdanjou.fr
+      </h1>
+      <CardButton />
+    </CardLink>
+  </Card>
+</template>
+
+<style scoped lang="scss">
+.title {
+  @apply text-3xl font-bolder mb-8;
+}
+
+.subtitle {
+  @apply mt-4 mb-2 text-sm text-stone-500 dark:text-gray-400 uppercase;
+}
+</style>
