bca592872ff4bef297642d78449f2bed9ff64231
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-28 19:19:10 +0200

    Use CardContainer

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -11,40 +11,100 @@ useHead({
 <template>
   <section>
     <PageTitle title="My Shelf" />
-    <div class="flex justify-center">
-      <div class="grid grid-cols-2 gap-6 grid-flow-row-dense auto-">
-        <Card>
-          <CardLink href="/blog/123">
-            <h1 class="text-3xl font-bold" :class="getTextColor()">
-              TITLE
-            </h1>
-            <h3 class="text-sm text-stone-500 dark:text-gray-400 mt-2 text-justify">
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-              DESCRIPTION
-            </h3>
-            <div class="flex items-center justify-between mt-4">
-              <button
-                class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
+    <CardContainer class="grid-cols-2">
+      <Card>
+        <CardLink href="/blog/123">
+          <h1 class="text-3xl font-bold" :class="getTextColor()">
+            TITLE
+          </h1>
+          <h3 class="text-sm text-stone-500 dark:text-gray-400 mt-2 text-justify">
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+          </h3>
+          <div class="flex items-center justify-between mt-4">
+            <button
+              class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
                 bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
-              >
-                <div class="flex items-center justify-center flex items-center justify-center">
-                  Read more <PlusIcon class="ml-2" />
-                </div>
-              </button>
-              <p class="ml-4" :class="getTextColor()">
-                6 Feb. 2003
-              </p>
-            </div>
-          </CardLink>
-        </Card>
-      </div>
-    </div>
+            >
+              <div class="flex items-center justify-center flex items-center justify-center">
+                Read more <PlusIcon class="ml-2" />
+              </div>
+            </button>
+            <p class="ml-4" :class="getTextColor()">
+              6 Feb. 2003
+            </p>
+          </div>
+        </CardLink>
+      </Card>
+      <Card>
+        <CardLink href="/blog/123">
+          <h1 class="text-3xl font-bold" :class="getTextColor()">
+            TITLE
+          </h1>
+          <h3 class="text-sm text-stone-500 dark:text-gray-400 mt-2 text-justify">
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+          </h3>
+          <div class="flex items-center justify-between mt-4">
+            <button
+              class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
+                bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
+            >
+              <div class="flex items-center justify-center flex items-center justify-center">
+                Read more <PlusIcon class="ml-2" />
+              </div>
+            </button>
+            <p class="ml-4" :class="getTextColor()">
+              6 Feb. 2003
+            </p>
+          </div>
+        </CardLink>
+      </Card>
+      <Card>
+        <CardLink href="/blog/123">
+          <h1 class="text-3xl font-bold" :class="getTextColor()">
+            TITLE
+          </h1>
+          <h3 class="text-sm text-stone-500 dark:text-gray-400 mt-2 text-justify">
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+            DESCRIPTION
+          </h3>
+          <div class="flex items-center justify-between mt-4">
+            <button
+              class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
+                bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
+            >
+              <div class="flex items-center justify-center flex items-center justify-center">
+                Read more <PlusIcon class="ml-2" />
+              </div>
+            </button>
+            <p class="ml-4" :class="getTextColor()">
+              6 Feb. 2003
+            </p>
+          </div>
+        </CardLink>
+      </Card>
+    </CardContainer>
   </section>
 </template>
--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -1,11 +1,32 @@
 <script setup lang="ts">
+import { useTheme } from '~/composables/useTheme'
+
+const { getTextColor } = useTheme()
+
 useHead({
   title: 'My work - Arthur Danjou',
 })
 </script>
 
 <template>
-  <main>
+  <section>
     <PageTitle title="All Works" />
-  </main>
+    <CardContainer>
+      <Card>
+        <CardLink href="/blog/123">
+          <div>
+            <img />
+          </div>
+          <div>
+            <h3 class="text-sm text-stone-500 dark:text-gray-400 mb-2">
+              Dev, Personal
+            </h3>
+            <h1 class="text-3xl font-bold" :class="getTextColor()">
+              Erisium
+            </h1>
+          </div>
+        </CardLink>
+      </Card>
+    </CardContainer>
+  </section>
 </template>
