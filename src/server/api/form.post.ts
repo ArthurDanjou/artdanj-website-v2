965350b076a8462a63c3eeb502b9f73421cd70cb
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-29 19:53:17 +0200

    Add form in server route

---
+++ src/server/api/form.post.ts
@@ -0,0 +1,17 @@
+import { defineEventHandler, readBody } from 'h3'
+import { PrismaClient } from '@prisma/client'
+
+export default defineEventHandler(async (event) => {
+  const client = new PrismaClient()
+  const body = await readBody(event)
+  const form = await client.form.create({
+    data: {
+      name: body.name,
+      email: body.email,
+      content: body.content,
+    },
+  })
+  return {
+    code: form !== null ? 200 : 500,
+  }
+})
