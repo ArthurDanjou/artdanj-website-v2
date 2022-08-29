afdf29d798e013616cc5be2212383f279e1e767d
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-29 19:53:39 +0200

    Increase footer margin bottom

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,7 @@ const { getBackgroundColor } = useTheme()
 </script>
 
 <template>
-  <footer class="flex items-center justify-center mt-18 mb-36">
+  <footer class="flex items-center justify-center mt-18 mb-44">
     <div class="w-full mx-32 duration-300" :class="getBackgroundColor()">
       Footer yeah
     </div>
