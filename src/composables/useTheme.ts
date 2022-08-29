65302f51d1216779c5270eeae4d01af820f9a2c9
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-29 17:59:13 +0200

    Export theme in useTheme

--- src/composables/useTheme.ts
+++ src/composables/useTheme.ts
@@ -2,14 +2,15 @@ import { useThemeStore } from '~/store/theme'
 import { getThemeBackgroundColor, getThemeTextColor } from '~/types/themes'
 
 export function useTheme() {
-  const themeStore = useThemeStore()
+  const getTheme = computed(() => useThemeStore().theme)
   const getTextColor = () => {
-    return getThemeTextColor(themeStore.theme)
+    return getThemeTextColor(getTheme.value)
   }
   const getBackgroundColor = () => {
-    return getThemeBackgroundColor(themeStore.theme)
+    return getThemeBackgroundColor(getTheme.value)
   }
   return {
+    getTheme,
     getTextColor,
     getBackgroundColor,
   }
