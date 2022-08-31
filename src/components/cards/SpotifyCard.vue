48d494858c2120430060effeb85684082ff6caa6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-08-31 15:55:03 +0200

    Hide animation if spotify isn't playing

--- src/components/cards/SpotifyCard.vue
+++ src/components/cards/SpotifyCard.vue
@@ -19,13 +19,13 @@ onMounted(() => {
       <div class="flex flex-col justify-center gap-y-4 lg:(flex-row gap-x-12)">
         <div class="flex items-center justify-center gap-x-8 lg:(flex-col gap-y-4)">
           <SpotifyIcon />
-          <div class="flex items-center justify-center h-30px">
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 0.85s infinite' : ''" />
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 1.26s infinite' : ''" />
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 0.62s infinite' : ''" />
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 1.11s infinite' : ''" />
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 0.79s infinite' : ''" />
-            <div class="play-indicator" :style="data.is_playing ? 'animation: playAnimation 1s infinite' : ''" />
+          <div v-if="data.is_playing" class="flex items-center justify-center h-30px">
+            <div class="play-indicator" style="animation: playAnimation 0.85s infinite" />
+            <div class="play-indicator" style="animation: playAnimation 1.26s infinite" />
+            <div class="play-indicator" style="animation: playAnimation 0.62s infinite" />
+            <div class="play-indicator" style="animation: playAnimation 1.11s infinite" />
+            <div class="play-indicator" style="animation: playAnimation 0.79s infinite" />
+            <div class="play-indicator" style="animation: playAnimation 1s infinite" />
           </div>
         </div>
         <div v-if="data.is_playing" class="flex flex-col justify-center items-center">
