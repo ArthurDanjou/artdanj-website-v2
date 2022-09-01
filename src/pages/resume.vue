c895cc566ecd0947e879eccfc15a96b00ed767d2
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-01 18:41:34 +0200

    Change main color text

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -12,18 +12,18 @@ useHead({
   <section>
     <PageTitle title="My Resume" />
     <div class="flex flex-col lg:flex-row">
-      <div class="w-full lg:w-1/3">
+      <div class="w-full lg:w-1/4">
         <div class="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-1 gap-6 auto-rows-fr">
           <Card>
             <CardDiv>
-              <img class="border-2 border-dark rounded-full w-1/3" src="~/assets/images/MeHappy.png" alt="Image of me">
-              <h1 class="font-bold text-3xl" :class="getTextColor()">
+              <img class="mb-4 w-1/2" src="~/assets/images/MeHappy.png" alt="Image of me">
+              <h1 class="font-bold text-3xl">
                 Arthur Danjou
               </h1>
-              <h3 class="text-lg text-gray-600 dark:text-gray-400">
+              <h3 class="text-xl text-gray-600 dark:text-gray-400" :class="getTextColor()">
                 Software Engineer
               </h3>
-              <p class="text-sm text-gray-600 dark:text-gray-400">
+              <p class="text-md text-gray-600 dark:text-gray-400">
                 Paris, France
               </p>
             </CardDiv>
@@ -35,7 +35,7 @@ useHead({
           </Card>
         </div>
       </div>
-      <div class="w-full mt-16 lg:(w-2/3 ml-16 mt-0)">
+      <div class="w-full mt-16 lg:(w-3/4 ml-16 mt-0)">
         <ResumeTitle title="About Me" />
         <ResumeSection>
           <p class="text-gray-600 dark:text-gray-400 text-justify leading-6">
