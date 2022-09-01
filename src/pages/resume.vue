72bb93fdc8a4c2b2a8e76319d4d580afa05e7908
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-01 17:55:29 +0200

    Working on resume page

--- src/pages/resume.vue
+++ src/pages/resume.vue
@@ -17,7 +17,7 @@ useHead({
           <h1 class="font-bold text-3xl" :class="getTextColor()">
             Arthur Danjou
           </h1>
-          <h3 class="mt-2 mb-4 text-lg text-gray-600 dark:text-gray-400">
+          <h3 class="text-lg text-gray-600 dark:text-gray-400">
             Software Engineer
           </h3>
           <p class="text-sm text-gray-600 dark:text-gray-400">
@@ -26,9 +26,13 @@ useHead({
           <ul />
         </CardDiv>
       </Card>
-      <Card>
+      <Card width="2">
         <CardDiv>
-          About me
+          <p class="text-gray-600 dark:text-gray-400 text-justify">
+            I am Arthur Danjou, a mathematics student at the Faculty of Sciences of Paris-Saclay but also a freelance software engineer. I am very interested in new technologies, development and computing. I am able to quickly learn new technologies to meet the needs of different projects. I am always motivated by a challenge and like to be well organized to produce consistent results. Being able to create software from scratch is a real source of motivation.
+
+            I love sharing my knowledge and helping others. I do a lot of research and read a lot of technical articles to discover new features. As long as I share my passions, I will continue to do this.
+          </p>
         </CardDiv>
       </Card>
       <Card>
