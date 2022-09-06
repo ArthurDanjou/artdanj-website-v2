a06329463285135e863da23b211ea240aae8ea7f
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-06 19:43:30 +0200

    Add translation for blog page title

--- src/pages/blog/index.vue
+++ src/pages/blog/index.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import { useTheme } from '~/composables/useTheme'
+import { useI18n } from 'vue-i18n'
 
 const { getTextColor } = useTheme()
 
+const { t } = useI18n()
+
 useHead({
-  title: 'My blog - Arthur Danjou',
+  title: t('head.blog'),
 })
 </script>
 
