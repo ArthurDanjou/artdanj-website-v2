119371e46f9ac0cb3955d01f48ca122dbe466e7c
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-06 19:39:50 +0200

    Use cookie when loading i18n

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,19 +1,20 @@
 import { createI18n } from 'vue-i18n'
 import fr from '../locales/fr-FR.json'
 import en from '../locales/en-EN.json'
-import { defineNuxtPlugin } from '#app'
+import {defineNuxtPlugin} from '#app'
+import {useLang} from "~/composables/useLang";
 
 export default defineNuxtPlugin(({ vueApp }) => {
+  const { getLang } = useLang()
   const i18n = createI18n({
     legacy: false,
-    locale: 'en',
     fallbackLocale: 'en',
+    locale: getLang(),
     globalInjection: true,
     messages: {
       fr,
       en,
     },
-
   })
 
   vueApp.use(i18n)
