5aa4a441f70c35fdd77d7138b18892a7e66d6ee4
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-12 21:44:31 +0200

    Rename component

---
+++ src/components/DateTag.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { formatDate } from '~/logic/date'
+
+defineProps({
+  startDate: String,
+  endDate: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<template>
+  <div v-if="startDate !== endDate" class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+    {{ formatDate(startDate) }} — {{ formatDate(endDate) }}
+  </div>
+  <div v-else class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+    {{ formatDate(endDate) }}
+  </div>
+</template>
