485d6c1d9a9d1dc07bbac9cfe3d28ed8d9758f3f
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 21:40:48 +0200

    Change margin

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -12,7 +12,7 @@ const { t } = useI18n()
 </script>
 
 <template>
-  <h1 class="font-bold text-gray-200 dark:text-zinc-800 text-6xl md:text-8xl lg:text-14xl text-center mb-16">
+  <h1 class="font-bold text-gray-200 dark:text-zinc-800 text-6xl md:text-8xl lg:text-14xl text-center mt-6 mb-8">
     {{ t(`titles.${title}`) }}
   </h1>
 </template>
