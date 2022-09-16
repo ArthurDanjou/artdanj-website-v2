460073dbd668427eb649ae995bc1616d19f7d6b6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 21:35:02 +0200

    Trying to import theme color to post

--- src/pages/blog/[id].vue
+++ src/pages/blog/[id].vue
@@ -1,13 +1,15 @@
 <script setup lang="ts">
 import { useRoute } from '#app'
+import { useTheme } from '#imports'
 
 const route = useRoute()
+const { getTextColor } = useTheme()
 </script>
 
 <template>
   <section class="md:w-2/3 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-dark-800 sm:rounded">
-    <main class="max-w-2xl px-4 mx-auto prose sm:px-8 prose-gray dark:prose-invert">
-      <ContentDoc class="prose prose-xl" :path="`/posts/${route.params.id}`">
+    <main class="max-w-2xl px-4 mx-auto sm:px-8">
+      <ContentDoc id="prose" class="prose dark:prose-invert" :path="`/posts/${route.params.id}`">
         <template #not-found>
           <h1>Not found</h1>
         </template>
@@ -15,3 +17,12 @@ const route = useRoute()
     </main>
   </section>
 </template>
+
+<style scoped lang="scss">
+#prose {
+  // todo add theme for blog post
+  h1 {
+    @apply v-bind(getTextColor()) bg-red-300
+  }
+}
+</style>
