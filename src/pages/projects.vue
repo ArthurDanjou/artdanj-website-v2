e312e9ff67ea99ea546bd235610751299233e25e
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 22:47:46 +0200

    Use tag

--- src/pages/projects.vue
+++ src/pages/projects.vue
@@ -18,12 +18,17 @@ useHead({
             <img>
           </div>
           <div>
-            <h3 class="text-sm text-gray-600 dark:text-gray-400 mb-2">
-              Dev, Personal
-            </h3>
             <h1 class="text-3xl font-bold">
               Erisium
             </h1>
+            <h3 class="text-sm text-gray-600 dark:text-gray-400 mb-2 space-x-1">
+              <div class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                Dev
+              </div>
+              <div class="mb-1 translate-y-px inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+                Personnal
+              </div>
+            </h3>
           </div>
         </CardLink>
       </Card>
