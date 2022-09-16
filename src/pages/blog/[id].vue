b2f7ef4df007787c88c3c9be7a5feba7524bda2f
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 22:48:01 +0200

    Add margin

--- src/pages/blog/[id].vue
+++ src/pages/blog/[id].vue
@@ -5,7 +5,7 @@ const route = useRoute()
 </script>
 
 <template>
-  <section class="md:w-2/3 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-dark-800 sm:rounded">
+  <section class="mt-12 md:w-2/3 py-10 m-auto bg-white sm:px-8 sm:shadow dark:bg-dark-800 sm:rounded">
     <main class="max-w-2xl px-4 mx-auto sm:px-8">
       <ContentDoc id="prose" class="prose dark:prose-invert" :path="`/posts/${route.params.id}`">
         <template #not-found>
