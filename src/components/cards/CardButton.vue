814d12b7573db84b87e525afd970a8947644eb51
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 21:37:44 +0200

    Change bg color

--- src/components/cards/CardButton.vue
+++ src/components/cards/CardButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div
-    class="flex items-center justify-center  p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
-    bg-white text-stone-400 dark:(bg-dark-800 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
+    class="flex items-center justify-center p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
+    bg-white text-stone-400 dark:(bg-dark-900 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
   >
     <Icon name="ph:arrow-circle-right-bold" size="32px" />
   </div>
