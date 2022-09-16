68f393b3c3b0fc3730faeb9d3f7314509e56706b
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-16 21:35:44 +0200

    Add CardIcon component and StatsCard component

---
+++ src/components/cards/CardIcon.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+defineProps({
+  stick: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <div class="flex" :class="stick ? '' : 'mb-8'">
+    <div class="p-1 rounded-lg bg-stone-100 dark:bg-dark-600 shadow-dark">
+      <slot />
+    </div>
+  </div>
+</template>
---
+++ src/components/cards/StatsCard.vue
@@ -0,0 +1,7 @@
+<template>
+  <Card>
+    <CardDiv class="flex items-center !justify-center">
+      <slot />
+    </CardDiv>
+  </Card>
+</template>
