32ab401d3d39e9a45d17fce1f55609bc322744d7
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-18 14:41:10 +0200

    Working on merge vue i18n

--- src/components/cards/AnnounceCard.vue
+++ src/components/cards/AnnounceCard.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
 import { useI18n } from 'vue-i18n'
+import { useAsyncData } from '#imports'
+import type { Announcement } from '~/types/types'
 
-const { t, setLocaleMessage } = useI18n()
-const { data } = await useAsyncData('announce', async () => {
-  const announce = await $fetch('/api/announces')
-  setLocaleMessage('en', {
-    message: data.value.english_content,
+const { t, mergeLocaleMessage } = useI18n()
+const { data: announce } = await useAsyncData('announce', async () => {
+  const announce = await $fetch<Announcement>('/api/announces')
+  mergeLocaleMessage('en', {
+    [announce.title]: announce.english_content,
   })
-  setLocaleMessage('fr', {
-    message: data.value.french_content,
+  mergeLocaleMessage('fr', {
+    [announce.title]: announce.french_content,
   })
   return announce
 })
@@ -21,8 +23,8 @@ const { data } = await useAsyncData('announce', async () => {
         <Icon name="tabler:speakerphone" size="42px" />
       </CardIcon>
       <div class="flex flex-col space-y-4">
-        <h1 v-if="data" class="title">
-          {{ t(data.title) }}
+        <h1 v-if="announce" class="title">
+          {{ t(announce.title) }}
         </h1>
         <h1 v-else class="title">
           {{ t('cards.announces.default') }}
