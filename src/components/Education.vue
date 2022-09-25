ee4bd670d419540e13f72db0438ccac10492118b
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-25 18:11:31 +0200

    Add gray colors to description

--- src/components/Education.vue
+++ src/components/Education.vue
@@ -15,7 +15,8 @@ const { t } = useI18n()
     <div>
       <DateTag :start-date="education.startDate" :end-date="education.endDate" />
       <h1 class="text-lg leading-5">
-        <strong>{{ t(education.title.code) }}</strong> — {{ education.location }}
+        <strong>{{ t(education.title.code) }}</strong>
+        <span class="text-gray-600 dark:text-gray-400"> — {{ education.location }}</span>
       </h1>
     </div>
     <p class="text-justify text-md leading-5 text-gray-600 dark:text-gray-400">
