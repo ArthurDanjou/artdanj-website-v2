0e199a9108914aab78decd50b29ffd441776adf2
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-30 18:56:09 +0200

    Fix typo

--- src/components/cards/BlogCard.vue
+++ src/components/cards/BlogCard.vue
@@ -24,15 +24,14 @@ defineProps({
       </div>
       <div class="flex items-center justify-between mt-8">
         <p>
-          {{ post.publishedAt }}<br>
-          <span class="text-gray-600 dark:text-gray-400">{{ post.readingMins }} min.</span>
+          {{ post.publishedAt }} • {{ post.readingMins }} min read
         </p>
         <div
           class="px-4 py-2 duration-300 cursor-pointer border-2 rounded-full border-dark
                 bg-white text-stone-400 dark:(bg-dark-900 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
         >
-          <div class="flex items-center justify-center flex items-center justify-center">
-            Read more<Icon name="material-symbols:add-circle-outline-rounded" size="20px" class="ml-1" />
+          <div class="flex items-center justify-center font-medium">
+            Read more <Icon name="material-symbols:add-circle-outline-rounded" size="24px" class="ml-1" />
           </div>
         </div>
       </div>
