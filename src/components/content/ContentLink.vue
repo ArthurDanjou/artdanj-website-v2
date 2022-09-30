e290f803c166dfdb3c95e72c7f3af334779e6e39
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-30 18:28:30 +0200

    Remove theme color for content link component

--- src/components/content/ContentLink.vue
+++ src/components/content/ContentLink.vue
@@ -8,44 +8,23 @@ defineProps({
     type: String,
     default: '_parent',
     enum: ['_blank', '_parent', '_self', '_top'],
-  },
-  content: {
-    type: String,
-  },
+  }
 })
 
 const getColor = computed(() => {
-  switch ('') {
-    case 'sky':
-      return 'decoration-sky-500 hover:text-sky-500 hover:decoration-sky-500/30 focus:text-sky-500 focus:ring-sky-500/40\n'
-          + 'dark:(decoration-sky-400 hover:text-sky-400 hover:decoration-sky-400/30 focus:text-sky-400 focus:ring-sky-400/40)'
-    case 'orange':
-      return 'decoration-orange-500 hover:text-orange-500 hover:decoration-orange-500/30 focus:text-orange-500 focus:ring-orange-500/40\n'
-          + 'dark:(decoration-orange-400 hover:text-orange-400 hover:decoration-orange-400/30 focus:text-orange-400 focus:ring-orange-400/40)'
-    case 'yellow':
-      return 'decoration-yellow-500 hover:text-yellow-500 hover:decoration-yellow-500/30 focus:text-yellow-500 focus:ring-yellow-500/40\n'
-          + 'dark:(decoration-yellow-400 hover:text-yellow-400 hover:decoration-yellow-400/30 focus:text-yellow-400 focus:ring-yellow-400/40)'
-    case 'lime':
-      return 'decoration-lime-500 hover:text-lime-500 hover:decoration-lime-500/30 focus:text-lime-500 focus:ring-lime-500/40\n'
-          + 'dark:(decoration-lime-400 hover:text-lime-400 hover:decoration-lime-400/30 focus:text-lime-400 focus:ring-lime-400/40)'
-    case 'violet':
-      return 'decoration-violet-500 hover:text-violet-500 hover:decoration-violet-500/30 focus:text-violet-500 focus:ring-violet-500/40\n'
-          + 'dark:(decoration-violet-400 hover:text-violet-400 hover:decoration-violet-400/30 focus:text-violet-400 focus:ring-violet-400/40)'
-    case 'red':
-      return 'decoration-red-500 hover:text-red-500 hover:decoration-red-500/30 focus:text-red-500 focus:ring-red-500/40\n'
-          + 'dark:(decoration-red-400 hover:text-red-400 hover:decoration-red-400/30 focus:text-red-400 focus:ring-red-400/40)'
-  }
+  return 'text-zinc-600 decoration-gray-500 hover:text-black hover:decoration-gray-500/30 focus:text-gray-500 focus:ring-gray-500/40\n'
+      + 'dark:(text-zinc-400 decoration-gray-300/30 hover:text-white hover:decoration-gray-400/30 hover:text-white focus:text-white focus:ring-gray-400/40)'
 })
 </script>
 
 <template>
   <NuxtLink
-    :href="link"
-    :target="target"
-    class="focusable rounded-sm font-medium text-zinc-800 dark:text-white
+      :href="link"
+      :target="target"
+      class="focusable rounded-sm font-bold
       underline decoration-2 underline-offset-2 font-bold duration-300"
-    :class="getColor"
+      :class="getColor"
   >
-    {{ content }}
+    <slot />
   </NuxtLink>
 </template>
