7cee9ece66c974536a5481ddd5efca8687a482fd
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-30 18:29:36 +0200

    Remove async

--- src/composables/useContent.ts
+++ src/composables/useContent.ts
@@ -1,48 +1,50 @@
 import type { Education, JsonParsedContent, Post, Project, SkillList, WorkExperience } from '~/types/content'
-import { queryContent, useAsyncData } from '#imports'
+import {useAsyncData} from "#app";
+import {queryContent} from "#imports";
 
-export const useProjects = async () => {
-  return useAsyncData('content:projects', async () => {
-      return await queryContent<JsonParsedContent<Project>>('projects').findOne()
+export const useProjects = () => {
+  return useAsyncData('content:projects',  () => {
+      return queryContent<JsonParsedContent<Project>>('projects').findOne()
   })
 }
 
 export const useEducations = () => {
-  return useAsyncData('content:educations', async () => {
-    return await queryContent<JsonParsedContent<Education[]>>('resume/educations').findOne()
+  return useAsyncData('content:educations',  () => {
+    return queryContent<JsonParsedContent<Education[]>>('resume/educations').findOne()
   })
 }
 
 export const useWorkExperiences = () => {
-  return useAsyncData('content:experiences', async () => {
-    return await queryContent<JsonParsedContent<WorkExperience[]>>('resume/experiences').findOne()
+  return useAsyncData('content:experiences',  () => {
+    return queryContent<JsonParsedContent<WorkExperience[]>>('resume/experiences').findOne()
   })
 }
 
 export const useSkills = () => {
-  return useAsyncData('content:skills', async () => {
-    return await queryContent<JsonParsedContent<SkillList[]>>('resume/skills').findOne()
+  return useAsyncData('content:skills',  () => {
+    return queryContent<JsonParsedContent<SkillList[]>>('resume/skills').findOne()
   })
 }
 
 export const usePosts = () => {
-  return useAsyncData('content:posts', async () =>
-    await queryContent<Post[]>('posts')
-      .only(['title', 'slug', 'description', 'publishedAt', 'cover', 'readingMins'])
-      .sort({
-        publishedAt: -1,
-      })
-      .find()
-  )
+  return useAsyncData('content:posts', () => {
+    return queryContent<Post[]>('posts')
+    .only(['title', 'slug', 'description', 'publishedAt', 'cover', 'readingMins'])
+    .sort({
+      publishedAt: -1,
+    })
+    .find()
+  })
 }
 
 export const useLatestPost = () => {
-  return useAsyncData('content:latestPost', () =>
-    queryContent<Post>('posts')
-      .sort({
-        publishedAt: -1,
-      })
-      .limit(1)
-      .findOne(),
-  )
+  return useAsyncData('content:latestPost', () => {
+    return queryContent<Post>('posts')
+    .only(['title', 'slug', 'description', 'publishedAt', 'cover', 'readingMins'])
+    .sort({
+      publishedAt: -1,
+    })
+    .limit(1)
+    .find()
+  })
 }
