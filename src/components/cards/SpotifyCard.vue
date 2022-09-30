523b9ab5da61c2d68cbf00698da5119e4253f7b6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-09-30 18:29:10 +0200

    Fix spotify component if error with the api

--- src/components/cards/SpotifyCard.vue
+++ src/components/cards/SpotifyCard.vue
@@ -16,8 +16,8 @@ onUnmounted(() => {
 </script>
 
 <template>
-  <Card v-if="data !== null" width="2">
-    <CardLink href="https://open.spotify.com/user/p3tavwpsi4zpz4xpmwlacwjoz" target="_blank">
+  <Card width="2">
+    <CardLink v-if="data !== null" href="https://open.spotify.com/user/p3tavwpsi4zpz4xpmwlacwjoz" target="_blank">
       <CardIcon>
         <Icon name="mdi:spotify" size="42px" />
       </CardIcon>
@@ -49,10 +49,27 @@ onUnmounted(() => {
       </p>
       <CardButton />
     </CardLink>
+    <CardDiv v-else>
+      <CardIcon>
+        <Icon style="animation: spin 2s infinite" name="ph:spinner-bold" size="42px" />
+      </CardIcon>
+      <div class="flex flex-col space-y-4">
+        <h1 class="title">Loading state...</h1>
+        <h3 class="subtitle">Fetching data from spotify</h3>
+      </div>
+    </CardDiv>
   </Card>
 </template>
 
 <style lang="scss">
+.title {
+  @apply text-3xl font-bold;
+}
+
+.subtitle {
+  @apply text-sm text-gray-600 dark:text-gray-400;
+}
+
 .play-indicator {
   @apply w-3px h-3px mr-3px rounded-3px bg-black dark:bg-white;
 }
@@ -66,6 +83,15 @@ onUnmounted(() => {
   }
   100% {
     height: 3px;
+  }
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
