6c4a5dc425cedb77d0ef4b36e42d618f6d0d1d45
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-02 20:03:44 +0200

    Move card button

--- src/components/cards/CardButton.vue
+++ src/components/cards/CardButton.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="flex items-center justify-center p-2 duration-300 cursor-pointer absolute bottom-4 right-4 border-2 rounded-full border-dark
+    class="flex items-center justify-center p-2 duration-300 cursor-pointer absolute bottom-3 right-3 border-2 rounded-full border-dark
     bg-white text-stone-400 dark:(bg-dark-900 text-stone-600) group-hover:(shadow-dark text-black) dark:group-hover:text-white"
   >
     <Icon name="ph:arrow-circle-right-bold" size="24px" />
