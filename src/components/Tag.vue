80dcf06f48724df062dc818a9d23e3a31d8398e7
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-02 19:40:48 +0200

    Create tag for projects

---
+++ src/components/Tag.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="translate-y-px tracking-widest inline-block flex-none rounded bg-zinc-200 p-1 text-xs font-medium leading-none text-zinc-500 dark:bg-zinc-800 dark:text-zinc-400">
+    <slot />
+  </div>
+</template>
