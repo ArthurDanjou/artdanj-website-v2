9125e873e481b60b318517559d2a66d95a136e3d
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-04 22:22:15 +0200

    Move create to upsert to edit message if already posted

--- src/server/api/guestbook.post.ts
+++ src/server/api/guestbook.post.ts
@@ -4,11 +4,17 @@ import { PrismaClient } from '@prisma/client'
 export default defineEventHandler(async (event) => {
   const client = new PrismaClient()
   const body = await readBody(event)
-  const guestBook = await client.guestBook.create({
-    data: {
+  const guestBook = await client.guestBook.upsert({
+    where: {
+      email: body.email,
+    },
+    create: {
       content: body.content,
       username: body.username,
       email: body.email,
+    },
+    update: {
+      content: body.content,
     },
   })
   await client.$disconnect()
