a2028c43dc7822f1077b25f6867de879ce8efbf8
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-04 22:22:43 +0200

    Add new logic date to format guestbook date

--- src/logic/date.ts
+++ src/logic/date.ts
@@ -1,5 +1,25 @@
+const MONTHS = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+]
+
 export const formatDate = (date: string) => {
   const split = date.split(' ')
 
   return date === 'Today' ? 'Today' : `${split[0]} ${split[1]}`
+}
+
+export const formatGuestBookDate = (date: Date) => {
+  const dateObj = new Date(date)
+  return `${dateObj.getDate()} ${MONTHS[dateObj.getMonth()]} ${dateObj.getFullYear()} at ${dateObj.getHours()}:${dateObj.getMinutes()}`
 }
