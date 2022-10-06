f3d0350fdfd8ddf1f7d98b0d4889d2ff3ca8812d
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-06 23:22:53 +0200

    Working on the maintenance page

--- src/pages/maintenance.vue
+++ src/pages/maintenance.vue
@@ -1,5 +1,24 @@
+<script setup lang="ts">
+import { definePageMeta, useHead } from '#imports'
+
+useHead({
+  title: 'Site under maintenance, please come back later - Arthur Danjou',
+})
+
+definePageMeta({
+  layout: 'maintenance',
+})
+</script>
+
 <template>
-  <div>
-    Hey, I'm the maintenance page.
-  </div>
+  <section class="h-full flex justify-center">
+    <div class="flex flex-col justify-center items-center text-center md:w-1/2">
+      <h1 class="text-2xl font-bold md:text-5xl my-4">
+        The website is under maintenance !
+      </h1>
+      <h3 class="text-lg md:text-xl text-gray-600 dark:text-gray-400">
+        Someone has kidnapped our website. We are negotiating with them to get it back as soon as possible.
+      </h3>
+    </div>
+  </section>
 </template>
