ca1a89fac799628789a1e2094bf811a190ee324d
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-06 23:23:45 +0200

    Use composable

--- src/pages/blog/[id].vue
+++ src/pages/blog/[id].vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import type { Post } from '~/types/content'
-import { queryContent, useAsyncData, useHead, useRoute } from '#imports'
+import { onMounted, queryContent, useAsyncData, useHead, useRoute } from '#imports'
+import { usePost } from '~/composables/usePost'
 
 const route = useRoute()
-const { data } = await useAsyncData(`blog:post:${route.params.id}`, () => queryContent<Post>(`/posts/${route.params.id}`).findOne())
+const { data: postContent } = await useAsyncData(`blog:post-content:${route.params.id}`, () => queryContent<Post>(`/posts/${route.params.id}`).findOne())
+const { like, getLikes, view, getViews } = await usePost(route.params.id)
+
+onMounted(() => {
+  view()
+})
 
 useHead({
-  title: `${data.value.title} - Arthur Danjou's shelf`,
+  title: `${postContent.value.title} - Arthur Danjou's shelf`,
 })
 </script>
 
@@ -14,25 +20,28 @@ useHead({
   <section class="mt-16 md:mt-32 md:w-2/3 mx-auto sm:rounded">
     <main class="max-w-2xl px-4 mx-auto sm:px-8">
       <h1 class="text-4xl font-bold">
-        {{ data.title }}
+        {{ postContent.title }}
       </h1>
       <h3 class="text-xl text-gray-600 dark:text-gray-400 my-2">
-        {{ data.description }}
+        {{ postContent.description }}
       </h3>
       <div class="flex justify-between text-sm text-gray-600 dark:text-gray-400">
         <div>
-          Arthur Danjou / {{ data.publishedAt }}
+          Arthur Danjou / {{ postContent.publishedAt }}
         </div>
         <div>
-          {{ data.readingMins }} min read • xxx.xxx.xxx views
+          {{ postContent.readingMins }} min read • {{ getViews }} views
         </div>
       </div>
       <div class="w-full bg-gray-300 my-4 rounded-md my-8">
-        {{ data.cover }}
+        {{ postContent.cover }}
       </div>
-      <ContentRenderer class="prose dark:prose-invert leading-6" :value="data" />
+      <ContentRenderer class="prose dark:prose-invert leading-6" :value="postContent" />
       <div class="my-8">
         Footer blog
+        <div @click.prevent="like()">
+          LIKE - {{ getLikes }}
+        </div>
       </div>
     </main>
   </section>
