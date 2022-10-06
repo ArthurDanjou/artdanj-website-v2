0670a22ca556196b869bba7b64a0aa8e08a1f6c6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-06 23:22:34 +0200

    Fetch api and compare the date

--- src/middleware/maintenance.global.ts
+++ src/middleware/maintenance.global.ts
@@ -1,17 +1,15 @@
-import { defineNuxtRouteMiddleware, navigateTo } from '#app'
+import type { Maintenance } from '~/types/types'
+import { defineNuxtRouteMiddleware, navigateTo, useAsyncData } from '#app'
 
 export default defineNuxtRouteMiddleware(async (to) => {
-  const isMaintenance = false // todo change
+  const today = new Date()
 
-  /*
-  const data = await $fetch<{ isMaintenance: boolean }>('https://api.arthurdanjou.fr/maintenance', {
-    headers: {
-      Authorization: `Bearer ${process.env.API_TOKEN}`,
-    },
+  const { data: maintenance } = await useAsyncData<Maintenance>('maintenance', () => {
+    return $fetch('/api/maintenance', {
+      method: 'GET',
+    })
   })
-  if (data)
-    isMaintenance = data.isMaintenance
-  */
+  const isMaintenance = maintenance.value && new Date(maintenance.value.beginAt).getTime() < today.getTime() && new Date(maintenance.value.endAt).getTime() > today.getTime()
 
   if (isMaintenance && to.name !== 'maintenance') {
     return navigateTo('/maintenance', {
