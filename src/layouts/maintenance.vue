d76d2378220b2a4dff9ec4658d123a3237ff98d6
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-10-06 23:22:41 +0200

    Add maintenance layout

---
+++ src/layouts/maintenance.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <NavBar />
+    <div class="height">
+      <slot />
+    </div>
+    <Footer />
+  </main>
+</template>
+
+<style>
+.height {
+  padding: 1rem;
+  height: calc(100vh - 165px)
+}
+
+@media (max-width: 768px) {
+  .height {
+    height: calc(100vh - 350px)
+  }
+}
+</style>
