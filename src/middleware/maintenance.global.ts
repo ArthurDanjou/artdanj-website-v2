232030fb3693b598033b6ab74abfe3f3e1352b92
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-11-04 19:18:19 +0100

    Fix error

--- src/middleware/maintenance.global.ts
+++ src/middleware/maintenance.global.ts
@@ -1,15 +1,13 @@
 import type { Maintenance } from '~/types/types'
-import { defineNuxtRouteMiddleware, navigateTo, useAsyncData } from '#app'
+import { defineNuxtRouteMiddleware, navigateTo } from '#app'
 
 export default defineNuxtRouteMiddleware(async (to) => {
   const today = new Date()
 
-  const { data: maintenance } = await useAsyncData<Maintenance>('maintenance', () => {
-    return $fetch('/api/maintenance', {
-      method: 'GET',
-    })
+  const maintenance = await $fetch<Maintenance>('/api/maintenance', {
+    method: 'GET',
   })
-  const isMaintenance = maintenance.value && new Date(maintenance.value.beginAt).getTime() < today.getTime() && new Date(maintenance.value.endAt).getTime() > today.getTime()
+  const isMaintenance = maintenance && new Date(maintenance.beginAt).getTime() < today.getTime() && new Date(maintenance.endAt).getTime() > today.getTime()
 
   if (isMaintenance && to.name !== 'maintenance') {
     return navigateTo('/maintenance', {
