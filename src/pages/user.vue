b60d88dc968f9b02f7d5baf240bfabee9fbbb122
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-11-18 16:30:05 +0100

    Use modal wrappers

--- src/pages/user.vue
+++ src/pages/user.vue
@@ -89,8 +89,8 @@ const questions = await getOwnQuestions()
       <client-only>
         <transition v-show="open" name="modal">
           <div>
-            <div class="z-101 h-300vh w-screen absolute inset-0 bg-black/40 overflow-y-auto" />
-            <div ref="modal" class="border border-dark bg-stone-100 text-black dark:(bg-dark-800 text-white) rounded-xl lg:w-1/3 z-102 fixed top-50 left-[50%] -translate-x-[50%] transform ">
+            <ModalBackground />
+            <ModalContainer ref="modal">
               <div class="flex flex-col">
                 <div class="rounded-t-xl p-4 border-b border-dark rounded-t-lg flex justify-between items-center">
                   <h1 class="font-bold text-xl">
@@ -108,20 +108,10 @@ const questions = await getOwnQuestions()
                   </div>
                 </div>
               </div>
-            </div>
+            </ModalContainer>
           </div>
         </transition>
       </client-only>
     </main>
   </section>
 </template>
-
-<style scoped lang="scss">
-.modal-enter-active, .modal-leave-active {
-  transition: opacity .3s;
-}
-.modal-enter-from, .modal-leave-to {
-  opacity: 0;
-  transform: translateX(-50%);
-}
-</style>
