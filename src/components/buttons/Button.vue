1c210dd14f45c3bb56aceb462e1963e49db56e8e
Arthur DANJOU <arthurdanjou@outlook.fr>
2022-12-14 23:26:26 +0100

    Improve button style

---
+++ src/components/buttons/Button.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+const props = defineProps({
+  content: String,
+  icon: String,
+  color: String,
+  thin: Boolean,
+  sendable: {
+    type: Boolean,
+    default: true,
+  },
+})
+
+const getColor = computed(() => {
+  switch (props.color) {
+    case 'red':
+      return 'bg-red-500 text-black hover:shadow-md hover:shadow-red-400 dark:hover:shadow-red-600 font-bold'
+    default:
+      return 'bg-stone-200 text-black hover:shadow-md hover:shadow-stone-300 dark:(bg-dark-400 text-white hover:shadow-dark-500)'
+  }
+})
+
+const getThickness = computed(() => {
+  return props.thin ? 'p-1' : 'p-2'
+})
+
+const isSendable = computed(() => {
+  if (props.sendable)
+    return 'hover:-translate-y-1 transform'
+  else
+    return 'cursor-not-allowed opacity-30 !shadow-none'
+})
+</script>
+
+<template>
+  <button class="rounded-xl duration-300" :class="[getColor, getThickness, isSendable]">
+    <div class="flex items-center space-x-1">
+      <Icon :name="icon" size="16" />
+      <p v-if="content">
+        {{ content }}
+      </p>
+    </div>
+  </button>
+</template>
---
+++ src/components/buttons/DeleteButton.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+defineProps({
+  content: String,
+  thin: {
+    type: Boolean,
+    default: false,
+  },
+})
+</script>
+
+<template>
+  <Button :content="content" icon="ph:trash-bold" color="red" :thin="thin" />
+</template>
